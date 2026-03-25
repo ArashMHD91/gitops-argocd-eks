e443d8b2acbef41c7ef1cd354dbb2765ff3885bf
ArashMHD91 <mhdarash91@gmail.com>
2026-03-25 12:57:14 +0100

    app: bump version to 1.0.1

--- app/app.py
+++ app/app.py
@@ -2,24 +2,19 @@ from flask import Flask, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
-# This automatically creates the /metrics endpoint
-# and tracks all request durations and counts
 metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def home():
     return jsonify({
         "app": "gitops-app",
-        "version": "1.0.0",
+        "version": "1.0.1",
         "status": "running",
         "message": "GitOps with ArgoCD on EKS"
     })
 
 @app.route("/health")
 def health():
-    # Kubernetes liveness & readiness probe hits this
-    # Must return 200 for the pod to be considered healthy
     return jsonify({
         "status": "healthy"
     }), 200
